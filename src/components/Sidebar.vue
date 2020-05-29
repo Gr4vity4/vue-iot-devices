17a95bbfacce1c0576d671c84a81a1deeadc8e0f
Gr4vity4 <prawared.bw@gmail.com>
2020-05-29 15:08:51 +0700

    add mqtt package & mqtt subscribe

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -26,7 +26,7 @@
               <input type="text" class="input" ref="passwordInput">
             </div>
             <div class="field">
-              <label for="">Topic</label>
+              <label for="">Subscribe Topic</label>
               <input type="text" class="input" ref="topicInput" value="CMMC/#">
             </div>
             <div class="field" style="padding-top: 10px">
@@ -40,23 +40,62 @@
 </template>
 
 <script>
+  import {
+    mapState
+  } from 'vuex'
+  import Paho from 'paho-mqtt/mqttws31'
+
   export default {
     name: 'Sidebar',
     methods: {
       mqttConnect() {
         console.log('mqttConnect clicked')
+
         this.$store.dispatch('mqttConfig', {
           host: this.$refs.hostInput.value,
-          port: this.$refs.portInput.value,
+          port: parseInt(this.$refs.portInput.value),
           clientId: this.$refs.clientIdInput.value,
           username: this.$refs.usernameInput.value,
           password: this.$refs.passwordInput.value,
           topic: this.$refs.topicInput.value
         });
+
       }
     },
     mounted() {
       this.$refs.clientIdInput.value = `CMMC_${Math.random().toString(36).slice(-8)}`
+    },
+    computed: {
+      ...mapState(['mqttConfig'])
+    },
+    watch: {
+      mqttConfig: function (newValue) {
+        const client = new Paho.MQTT.Client(newValue.host, newValue.port, '', newValue.clientId)
+
+        client.onConnectionLost = onConnectionLost
+        client.onMessageArrived = onMessageArrived
+        client.connect({
+          onSuccess: onConnect
+        })
+
+        function onConnect() {
+          client.subscribe(newValue.topic)
+        }
+
+        function onConnectionLost(responseObject) {
+          if (responseObject.errorCode !== 0) {
+            console.log('onConnectionLost:' + responseObject.errorMessage)
+          }
+        }
+
+        function onMessageArrived(message) {
+          const data = JSON.parse(message.payloadString)
+
+          if (data.info !== undefined) {
+            console.log(data.info)
+          }
+        }
+      }
     }
   }
 
