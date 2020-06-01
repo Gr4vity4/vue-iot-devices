8024db79cade427668940e8c03c0f840405fbe5e
Gr4vity4 <prawared.bw@gmail.com>
2020-06-01 18:26:53 +0700

    add color picker & design update

--- src/components/DeviceControl.vue
+++ src/components/DeviceControl.vue
@@ -3,22 +3,19 @@
     <p class="title is-4">
       Device Control
     </p>
-    <!-- Destination Topic -->
+    <!-- Switch Control -->
     <div class="group">
       <div class="field">
-        <label for="">Destination Topic</label>
+        <label for="">Switch Control</label>
+      </div>
+      <div class="field">
         <input
           :value="prefix"
           class="input"
           type="text"
           placeholder="Destination Topic"
+          readonly
         >
-      </div>
-    </div>
-    <!-- Switch Control -->
-    <div class="group">
-      <div class="field">
-        <label for="">Switch Control</label>
       </div>
       <div class="field">
         <button
@@ -41,6 +38,15 @@
         <label for="">Slider Control</label>
       </div>
       <div class="field">
+        <input
+          :value="`${data.info.prefix}$/servo`"
+          class="input"
+          type="text"
+          placeholder="Destination Topic"
+          readonly
+        >
+      </div>
+      <div class="field">
         <vue-slider
           v-model="sliderValue"
           v-bind="sliderOptions"
@@ -55,14 +61,39 @@
         />
       </div>
     </div>
+    <!-- Color Picker -->
+    <div class="group">
+      <div class="field">
+        <label for="">Color Picker</label>
+      </div>
+      <div class="field">
+        <input
+          :value="`${data.info.prefix}$/neopixel`"
+          class="input"
+          type="text"
+          placeholder="Destination Topic"
+          readonly
+        >
+      </div>
+      <div class="field">
+        <photoshop-picker
+          v-model="colors"
+          value="#3299DC"
+        />
+      </div>
+      <div class="field">
+        <button
+          type="button"
+          class="button is-success"
+          @click="colorPicker()"
+          v-text="`Send`"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-  input {
-    margin-top: 6px !important;
-  }
-
   .group {
     margin-top: 20px;
   }
@@ -76,11 +107,13 @@
 import { mapState } from 'vuex';
 import VueSlider from 'vue-slider-component';
 import 'vue-slider-component/theme/antd.css';
+import { Photoshop } from 'vue-color';
 
 export default {
   name: 'DeviceControl',
   components: {
     'vue-slider': VueSlider,
+    'photoshop-picker': Photoshop,
   },
   data() {
     return {
@@ -88,7 +121,20 @@ export default {
       prefix: '',
       sliderValue: 0,
       sliderOptions: {
-        max: 255,
+        max: 180,
+      },
+      colors: {
+        hex: '#3299DC',
+        hsl: {
+          h: 150, s: 0.5, l: 0.2, a: 1,
+        },
+        hsv: {
+          h: 150, s: 0.66, v: 0.30, a: 1,
+        },
+        rgba: {
+          r: 25, g: 77, b: 51, a: 1,
+        },
+        a: 1,
       },
     };
   },
@@ -104,7 +150,10 @@ export default {
       window.client.publish(this.prefix, value);
     },
     sliderControl() {
-      window.client.publish(this.prefix, String(this.sliderValue));
+      window.client.publish(`${this.data.info.prefix}$/servo`, String(this.sliderValue));
+    },
+    colorPicker() {
+      window.client.publish(`${this.data.info.prefix}$/neopixel`, String(JSON.stringify(this.colors.rgba)));
     },
   },
 };
