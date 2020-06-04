18d32b64ddd5d430bba86b21e4afca52f14b556f
Gr4vity4 <prawared.bw@gmail.com>
2020-06-04 15:04:57 +0700

    add group between info & data

--- src/components/DeviceInfo.vue
+++ src/components/DeviceInfo.vue
@@ -1,35 +1,56 @@
 <template>
   <div>
-    <p class="title is-4">
-      Device Info
-    </p>
-    <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
-      <thead>
-        <tr>
-          <th>key</th>
-          <th>value</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="(value, propertyName, index) in data['info']"
-          :key="`${propertyName}-${index}`"
-        >
-          <td v-text="propertyName" />
-          <td v-text="value" />
-        </tr>
-
-        <tr
-          v-for="(value, propertyName, index) in data['d']"
-          :key="`${propertyName}-${index}`"
-        >
-          <td v-text="propertyName" />
-          <td v-text="value" />
-        </tr>
-      </tbody>
-    </table>
+    <div class="group">
+      <p class="title is-4">
+        Device Info
+      </p>
+      <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+        <thead>
+          <tr>
+            <th>key</th>
+            <th>value</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(value, propertyName, index) in data['info']"
+            :key="`${propertyName}-${index}`"
+          >
+            <td v-text="propertyName" />
+            <td v-text="value" />
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="group">
+      <p class="title is-4">
+        Device Data
+      </p>
+      <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+        <thead>
+          <tr>
+            <th>key</th>
+            <th>value</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(value, propertyName, index) in data['d']"
+            :key="`${propertyName}-${index}`"
+          >
+            <td v-text="propertyName" />
+            <td v-text="value" />
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
+<style scoped>
+.group {
+  margin-bottom: 20px;
+}
+</style>
 <script>
 import { mapState } from 'vuex';
 
